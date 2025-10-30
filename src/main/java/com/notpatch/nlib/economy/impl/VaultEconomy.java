efd97d7b6886f7e7258fb47ae9fd8fa967965f00
NotPatch <nitradosamet@hotmail.com>
2025-10-30 17:49:31 +0300

    Economy fix

--- src/main/java/com/notpatch/nlib/economy/impl/VaultEconomy.java
+++ src/main/java/com/notpatch/nlib/economy/impl/VaultEconomy.java
@@ -1,33 +1,57 @@
 package com.notpatch.nlib.economy.impl;
 
 import com.notpatch.nlib.NLib;
-import net.milkbowl.vault.economy.Economy;
+import com.notpatch.nlib.util.NLogger;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class VaultEconomy implements com.notpatch.nlib.economy.Economy {
 
-    private Economy economy = null;
+    private net.milkbowl.vault.economy.Economy economy = null;
 
-    public VaultEconomy(){
+    public VaultEconomy() {
         setupEconomy();
     }
 
     private void setupEconomy() {
         if (NLib.getInstance().getPlugin().getServer().getPluginManager().getPlugin("Vault") == null) {
+            NLogger.error("Vault plugin not found!");
             return;
         }
-        RegisteredServiceProvider<net.milkbowl.vault.economy.Economy> rsp = NLib.getInstance().getPlugin().getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
-        if (rsp == null) {
-            return;
+
+        try {
+            RegisteredServiceProvider<net.milkbowl.vault.economy.Economy> rsp =
+                    NLib.getInstance().getPlugin().getServer().getServicesManager()
+                            .getRegistration(net.milkbowl.vault.economy.Economy.class);
+
+            if (rsp == null) {
+                NLogger.error("No economy provider found!");
+                return;
+            }
+
+            economy = rsp.getProvider();
+            NLogger.info("Economy service initialized successfully!");
+        } catch (Exception e) {
+            NLogger.error("Failed to setup economy: " + e.getMessage());
         }
-        economy = rsp.getProvider();
+    }
+
+    @Override
+    public double getBalance(OfflinePlayer player) {
+        if (economy == null) {
+            NLogger.error("Economy service is null!");
+            return 0.0;
+        }
+        return economy.getBalance(player);
     }
 
     @Override
     public double withdraw(OfflinePlayer player, double price) {
-        if (economy != null) {
-            economy.withdrawPlayer(player, price);
+        if (economy == null) {
+            NLogger.error("Economy service is null!");
+            return 0.0;
+        }
+        if (economy.withdrawPlayer(player, price).transactionSuccess()) {
             return price;
         }
         return 0.0;
@@ -35,15 +59,13 @@ public class VaultEconomy implements com.notpatch.nlib.economy.Economy {
 
     @Override
     public double deposit(OfflinePlayer player, double price) {
-        if (economy != null) {
-            economy.depositPlayer(player, price);
+        if (economy == null) {
+            NLogger.error("Economy service is null!");
+            return 0.0;
+        }
+        if (economy.depositPlayer(player, price).transactionSuccess()) {
             return price;
         }
         return 0.0;
-    }
-
-    @Override
-    public double getBalance(OfflinePlayer player) {
-        return economy != null ? economy.getBalance(player) : 0.0;
     }
 }
